4b581bb5de761866f243b66086e09e41a46c9a14
Patrick Fowler <pat@outset.com>
2014-09-22 00:19:17 -0700

    Checkin code containing unirest, restify and node_rest_client code - looks like unirest is what i need as it handles COOKIES!

--- app/controllers/bgchecks.server.controller.js
+++ app/controllers/bgchecks.server.controller.js
@@ -8,13 +8,25 @@ var mongoose = require('mongoose'),
     Bgcheck = mongoose.model('Bgcheck'),
     Client = require('node-rest-client').Client, // TODO : Decide between this
     rest = require('restler'), // TODO: ... and this.
+    request = require('request'),
+    unirest = require('unirest'),
     _ = require('lodash');
 
 var client = new Client();
 
-var api_base_url = 'https://renovo-api-test.everifile.com/';
+var api_base_url = 'https://renovo-api-test.everifile.com/renovo';
 var username = 'api@dswheels.com';
 var password = 'Test#123';
+
+// Lets try 'request'!
+var login_request = function(req, res) {
+    var options = {
+        url: 'https://api.github.com/repos/mikeal/request',
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+};
 
 
 // Setup Client Methods (node-rest-client)
@@ -39,6 +51,9 @@ var EVerifileClient = rest.service(function(u, p) {
         return this.json('POST', login_path, post_data);
     },
     getAllApplicants: function() {
+        if (!!this.customHeaders) {
+            console.log('Getting Applicant list with headers: %o', this.customHeaders);
+        }
         if (!this.customHeaders) {
             console.log('Please login first');
         }
@@ -47,102 +62,69 @@ var EVerifileClient = rest.service(function(u, p) {
         });
     },
     getApplicant: function(id) {
+        if (!!this.customHeaders) {
+            console.log('Getting Applicant %d, with headers: %o', id, this.customHeaders);
+        }
+
         return this.get('/renovo/rest/applicant/' + id, {
             headers: this.customHeaders
         });
     },
 
-    setHeaders: function(headers) {
+    setCookie: function(headers) {
+        console.log('setting cookies based on headers: ', headers);
+
+        var set_cookies = headers['set-cookie'];
+
+        var cookies = [];
+
+        for (var i = 0; i < set_cookies.length; i++) {
+            var cookie = set_cookies[i];
+            console.log('adding cookie ', cookie);
+            cookies.push({
+                'Cookie': cookie
+            });
+        }
+
         this.customHeaders = headers;
     }
 });
 
 var eVerifile = new EVerifileClient(username, password);
 
-exports.login = function(req, res) {
+var CookieJar = unirest.jar(true);
 
-    eVerifile.login()
-        .on('complete', function(data, response) {
-            console.log('[Login] Response "COMPLETE" Callback: \n', data);
+var login_unirest = function(req, res) {
 
-            var responseUsername = (data.username || '').toLowerCase();
-
-            if (typeof(data) === 'string') {
-                console.error('Get All Applicants failed with HTML');
-                return res.status(400).send(data);
-            } else if (data.e) {
-                console.error('Login Failed: ', data);
-                return res.status(400).send({
-                    message: errorHandler.getDataErrorMessage(data),
-                    error: data.e
-                });
-            } else if (responseUsername.length > 0) {
-                console.log('Successful Login for user ', responseUsername);
-
-                console.log('setting request headers: ', response.headers);
-                eVerifile.setHeaders(response.headers);
-
-                res.jsonp(data);
-            }
-
-        })
-        .on('fail', function(data, response) {
-            console.log('[Login] Response "FAIL" Callback: \n', data);
-
-            return res.status(400).send({
-                message: errorHandler.getDataErrorMessage(data),
-                error: data.e
-            });
-        })
-        .on('error', function(err, response) {
-            console.error('[Login] Response "ERROR" Callback: ', err);
-            return res.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-            });
-        });
-};
-
-exports.login_node_rest_client = function(req, res) {
-
-    var args = {
-        data: {
-            'username': username,
-            'password': password
-        },
-        headers: {
-            'Content-Type': 'application/json'
-        }
+    var post_data = {
+        'username': username,
+        'password': password
     };
 
-    console.log('Posting w/ args: ', args);
+    unirest.post(api_base_url + '/rest/session')
+        .type('json')
+        .send(post_data)
+        .jar(CookieJar)
+        .end(function(response) {
+            debugger;
+            console.log(response.body);
 
-    client.methods.openSession(args, function(data, response) {
-        console.log('Default Callback');
-    })
-        .on('complete', function(data, response) {
-            console.log('Response "Complete" Callback');
-
-            var responseUsername = (data.username || '').toLowerCase();
-
-            if (data.e) {
-                console.error('Login Failed: ', data);
-                return res.status(400).send({
-                    message: errorHandler.getDataErrorMessage(data)
-                });
-            } else {
-                console.log('Successful Login for user ', responseUsername);
-                res.jsonp(data);
-            }
-
-        }).on('error', function(err) {
-            console.error('[Login] error: ', err);
-            return res.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-            });
+            res.jsonp(response.body);
         });
 };
 
-exports.getAllApplicants = function(req, res) {
+var getApplicants_unirest = function(req, res) {
+    unirest.get(api_base_url + '/rest/applicant')
+        .jar(CookieJar)
+        .end(function(response) {
+            debugger;
+            console.log(response.body);
+
+            res.jsonp(response.body);
+        });
+};
+
+var getApplicants_restler = function(req, res) {
     eVerifile.getAllApplicants()
         .on('complete', function(data, response) {
             if (typeof(data) === 'string') {
@@ -178,33 +160,131 @@ exports.getAllApplicants = function(req, res) {
                 message: errorHandler.getErrorMessage(err)
             });
         });
+};
 
-    //client.methods.getAllApplicants(function(data, response) {
-    //        if (data.e) {
-    //            console.error('Get All Applicants Failed: ', data);
-    //            return res.status(400).send({
-    //                message: errorHandler.getDataErrorMessage(data)
-    //            });
-    //        } else {
-    //            console.log('[getAllApplicants] response: ', response);
-    //            console.log('[getAllApplicants] data: "' + data + '"');//
+var getApplicants_node_rest_client = function(req, res) {
+    client.methods.getAllApplicants(function(data, response) {
+        if (data.e) {
+            console.error('Get All Applicants Failed: ', data);
+            return res.status(400).send({
+                message: errorHandler.getDataErrorMessage(data)
+            });
+        } else {
+            console.log('[getAllApplicants] response: ', response);
+            console.log('[getAllApplicants] data: "' + data + '"'); //
 
-    //            if (data.length > 0) {
-    //                res.jsonp(data);
-    //            } else {
-    //                res.jsonp({
-    //                    message: 'missing data response',
-    //                    'response': JSON.stringify(response, errorHandler.censor(response))
-    //                });
-    //            }
-    //        }//
+            if (data.length > 0) {
+                res.jsonp(data);
+            } else {
+                res.jsonp({
+                    message: 'missing data response',
+                    'response': JSON.stringify(response, errorHandler.censor(response))
+                });
+            }
+        }
 
-    //    }).on('error', function(err) {
-    //        console.error('[getAllApplicants] error: ', err);
-    //        return res.status(400).send({
-    //            message: errorHandler.getErrorMessage(err)
-    //        });
-    //    });
+    }).on('error', function(err) {
+        console.error('[getAllApplicants] error: ', err);
+        return res.status(400).send({
+            message: errorHandler.getErrorMessage(err)
+        });
+    });
+}
+
+var login_restler = function(req, res) {
+
+    eVerifile.login()
+        .on('complete', function(data, response) {
+            console.log('[Login] Response "COMPLETE" Callback: \n', data);
+
+            var responseUsername = (data.username || '').toLowerCase();
+
+            if (typeof(data) === 'string') {
+                console.error('Get All Applicants failed with HTML');
+                return res.status(400).send(data);
+            } else if (data.e) {
+                console.error('Login Failed: ', data);
+                return res.status(400).send({
+                    message: errorHandler.getDataErrorMessage(data),
+                    error: data.e
+                });
+            } else if (responseUsername.length > 0) {
+                console.log('Successful Login for user ', responseUsername);
+
+                eVerifile.setCookie(response.headers);
+
+                res.jsonp(data);
+            }
+
+        })
+        .on('fail', function(data, response) {
+            console.log('[Login] Response "FAIL" Callback: \n', data);
+
+            return res.status(400).send({
+                message: errorHandler.getDataErrorMessage(data),
+                error: data.e
+            });
+        })
+        .on('error', function(err, response) {
+            console.error('[Login] Response "ERROR" Callback: ', err);
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        });
+};
+
+var login_node_rest_client = function(req, res) {
+
+    var args = {
+        data: {
+            'username': username,
+            'password': password
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    console.log('Posting w/ args: ', args);
+
+    client.methods.openSession(args, function(data, response) {
+        console.log('Response "Complete" Callback');
+
+        var responseUsername = (data.username || '').toLowerCase();
+
+        if (data.e) {
+            console.error('Login Failed: ', data);
+            return res.status(400).send({
+                message: errorHandler.getDataErrorMessage(data)
+            });
+        } else {
+            debugger;
+            console.log('Successful Login for user ', responseUsername);
+            res.jsonp(data);
+        }
+
+    })
+        .on('error', function(err) {
+            console.error('[Login] error: ', err);
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        });
+};
+
+exports.getAllApplicants = function(req, res) {
+    // return getApplicants_node_rest_client(req, res);
+    // return getApplicants_restler(req, res);
+    return getApplicants_unirest(req, res);
+
+};
+
+
+exports.login = function(req, res) {
+    //return login_restler(req, res);
+    //return login_node_rest_client(req, res);
+    return login_unirest(req, res);
+    //return login_request(req, res);
 };
 
 exports.getApplicant = function(req, res, next, _id) {
