c3aa768ebbf93009e6d7b0b086c2049e7f07fae0
Patrick Fowler <pat@outset.com>
2014-09-21 20:36:16 -0700

    Progress checkpoint #1 for bg check logic

--- app/routes/bgchecks.server.routes.js
+++ app/routes/bgchecks.server.routes.js
@@ -1,19 +1,31 @@
 'use strict';
 
 module.exports = function(app) {
-	var users = require('../../app/controllers/users');
-	var bgchecks = require('../../app/controllers/bgchecks');
+    var users = require('../../app/controllers/users');
+    var bgchecks = require('../../app/controllers/bgchecks');
 
-	// Bgchecks Routes
-	app.route('/bgchecks')
-		.get(bgchecks.list)
-		.post(users.requiresLogin, bgchecks.create);
+    // Bgchecks Routes
+    app.route('/bgchecks')
+        .get(bgchecks.list)
+        .post(users.requiresLogin, bgchecks.create);
 
-	app.route('/bgchecks/:bgcheckId')
-		.get(bgchecks.read)
-		.put(users.requiresLogin, bgchecks.hasAuthorization, bgchecks.update)
-		.delete(users.requiresLogin, bgchecks.hasAuthorization, bgchecks.delete);
+    app.route('/bgchecks/:bgcheckId')
+        .get(bgchecks.read)
+        .put(users.requiresLogin, bgchecks.hasAuthorization, bgchecks.update)
+        .delete(users.requiresLogin, bgchecks.hasAuthorization, bgchecks.delete);
 
-	// Finish by binding the Bgcheck middleware
-	app.param('bgcheckId', bgchecks.bgcheckByID);
+    // DEBUGGING Routes
+    app.route('/bgcheck/login')
+        .get(bgchecks.login);
+
+    app.route('/bgcheck/applicants')
+        .get(bgchecks.getAllApplicants);
+
+    app.route('/bgcheck/applicants/:applicantId')
+        .get(bgchecks.read);
+
+    app.param('applicantId', bgchecks.getApplicant);
+
+    // Finish by binding the Bgcheck middleware
+    app.param('bgcheckId', bgchecks.bgcheckByID);
 };
