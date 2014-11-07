5fafa92eed46b59206596d73b2323870dae02bcb
Patrick Fowler <pfowler@cs.stanford.edu>
2014-11-07 12:57:27 -0800

    Tweaking the directives

--- public/modules/users/directives/os-profile.client.directive.js
+++ public/modules/users/directives/os-profile.client.directive.js
@@ -1,19 +1,35 @@
 'use strict';
 
-angular.module('users').directive('osProfile', [
-    function() {
-        return {
-            scope: {
-                profile: '=',
-                editMode: '=',
-                enableEdit: '@'
-            },
-            //template: '<h1>HELLO!!!</h1>',
-            templateUrl: 'modules/users/views/templates/user-profile.client.template.html',
-            restrict: 'E',
-            replace: true,
-            controller: 'ProfileController',
-            controllerAs: 'ctrl'
-        };
-    }
-]);
+angular.module('users')
+    .directive('osProfile', [
+        function() {
+            return {
+                scope: {
+                    profile: '=',
+                    editMode: '=',
+                },
+                templateUrl: 'modules/users/views/templates/user-profile.client.template.html',
+                restrict: 'E',
+                replace: true,
+                controller: 'OsProfileController',
+                controllerAs: 'ctrl',
+                //bindToController: true
+            };
+        }
+    ])
+    .controller('OsProfileController',
+        function($scope) {
+            debugger;
+            console.log('[OPC] Profile: %o', this.profile);
+            console.log('[OPC] editMode: %o', this.editMode);
+            this.edit = function() {
+                console.log('[OsProfileController] edit()');
+                this.editMode.enabled = true;
+            };
+
+            this.cancel = function() {
+                console.log('[OsProfileController] cancel()');
+                this.editMode.enabled = false;
+            };
+        }
+    );
