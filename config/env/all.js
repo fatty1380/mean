2abd5847e33c33ba915f8aeb47894c0b6dc18d6c
Patrick Fowler <pfowler@cs.stanford.edu>
2014-11-19 13:43:34 -0800

    Added "autofill-event" polyfill to resolve issue with safari (and other browser's) auto-fill events not firing. See https://github.com/tbosch/autofill-event for more information.

--- config/env/all.js
+++ config/env/all.js
@@ -1,48 +1,49 @@
 'use strict';
 
 module.exports = {
-	app: {
+    app: {
         title: 'outset',
         description: 'Driving Connections',
         keywords: 'MongoDB, Express, AngularJS, Node.js'
-	},
-	port: process.env.PORT || 3000,
-	secure: process.env.SECURE || false,
-	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
-	sessionCollection: 'sessions',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+    },
+    port: process.env.PORT || 3000,
+    secure: process.env.SECURE || false,
+    templateEngine: 'swig',
+    sessionSecret: 'MEAN',
+    sessionCollection: 'sessions',
+    assets: {
+        lib: {
+            css: [
+                'public/lib/bootstrap/dist/css/bootstrap.css',
+                'public/lib/bootstrap/dist/css/bootstrap-theme.css',
                 'public/lib/components-font-awesome/css/font-awesome.css',
-			],
-			js: [
-				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js',
-				'public/lib/angular-animate/angular-animate.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/angular-route/angular-route.js'
-			]
-		},
+            ],
+            js: [
+                'public/lib/angular/angular.js',
+                'public/lib/angular-resource/angular-resource.js',
+                'public/lib/angular-animate/angular-animate.js',
+                'public/lib/angular-ui-router/release/angular-ui-router.js',
+                'public/lib/angular-ui-utils/ui-utils.js',
+                'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+                'public/lib/angular-route/angular-route.js',
+                'public/lib/autofill-event/src/autofill-event.js'
+            ]
+        },
         less: [
             'public/modules/**/less/*.less'
         ],
-		css: [
-			'public/modules/**/css/*.css'
-		],
-		js: [
-			'public/config.js',
-			'public/application.js',
-			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
-		],
-		tests: [
-			'public/lib/angular-mocks/angular-mocks.js',
-			'public/modules/*/tests/*.js'
-		]
-	}
+        css: [
+            'public/modules/**/css/*.css'
+        ],
+        js: [
+            'public/config.js',
+            'public/application.js',
+            'public/modules/*/*.js',
+            'public/modules/*/*[!tests]*/*.js'
+        ],
+        tests: [
+            'public/lib/angular-mocks/angular-mocks.js',
+            'public/modules/*/tests/*.js'
+        ]
+    }
 };
