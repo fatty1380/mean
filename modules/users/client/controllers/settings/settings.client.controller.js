fe39f44865a6085163d1caf69028bca1294da8b6
Patrick Fowler <pfowler@cs.stanford.edu>
2014-11-26 14:56:19 -0800

    Moved the client settings controller to a more rational location, while splitting out the Profile controller (in the previous commit)

--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -1,11 +1,34 @@
-//'use strict';
-//
-//angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-//	function($scope, $http, $location, Users, Authentication) {
-//        debugger;
-//		$scope.user = Authentication.user;
-//
-//		// If user is not signed in then redirect back home
-//		if (!$scope.user) $location.path('/');
-//	}
-//]);
+/**
+ * SettingsController
+ * ------------------
+ * The Settings Controller contains very little logic on its own, and serves
+ * primarily as a gatekeeper to ensure that there is a user logged in, and that
+ * that user is allowed to access the settings page.
+ */
+
+(function() {
+
+    'use strict';
+
+    function SettingsController($scope, $http, $stateParams, $location, $log, Users, Authentication) {
+
+        this.user = Authentication.user;
+
+        // If user is not signed in then redirect back home
+        if (!this.user) $location.path('/');
+
+        if (!!$stateParams.userId) {
+            if ($stateParams.userId !== this.user._id && !this.user.isAdmin) {
+                $log.warn('[SettingsController] Non-Admin tried to access another user\'s settings');
+                $location.path('/');
+            }
+        }
+    }
+
+    SettingsController.$inject = ['$scope', '$http', '$stateParams', '$location', '$log', 'Users', 'Authentication'];
+
+    angular
+        .module('users')
+        .controller('SettingsController', SettingsController);
+
+})();
