73c02c77ee7ecded22df0a93d6e601a44a9500b4
Patrick Fowler <pfowler@cs.stanford.edu>
2014-12-01 12:21:52 -0800

    Refactored Directive and Directive Controller code to be Function/Injector format

--- modules/users/client/directives/login-modal.client.directive.js
+++ modules/users/client/directives/login-modal.client.directive.js
@@ -1,71 +1,78 @@
 (function() {
     'use strict';
 
-    angular.module('users')
-        .directive('loginModal', [
-            function() {
-                return {
-                    transclude: true,
-                    templateUrl: 'modules/users/views/templates/login-modal.client.template.html',
-                    restrict: 'EA',
-                    scope: {
-                        signin: '&',
-                        title: '@?'
-                    },
-                    controller: 'LoginModalController'
-                };
+    function SigninModalDirective() {
+        return {
+            transclude: true,
+            templateUrl: 'modules/users/views/templates/login-modal.client.template.html',
+            restrict: 'EA',
+            scope: {
+                signin: '&',
+                title: '@?'
+            },
+            controller: 'LoginModalController'
+        };
+    }
+
+    function SigninModalController($scope, $modal, $log) {
+
+        $scope.isOpen = false;
+
+        $scope.showLogin = function() {
+            var modalInstance = $modal.open({
+                templateUrl: 'loginModal.html',
+                controller: 'AuthenticationController'
+            });
+
+            modalInstance.result.then(function(result) {
+                $log.info('Modal result %o', result);
+                $scope.isOpen = false;
+            }, function(result) {
+                $log.info('Modal dismissed at: ' + new Date());
+                $scope.isOpen = false;
+            });
+
+            modalInstance.opened.then(function(args) {
+                $scope.isOpen = true;
+            });
+
+        };
+
+
+    }
+
+    function ModalFocusDirective($timeout, $parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.modalFocus);
+
+                scope.$watch(model, function(value) {
+                    console.log('value=', value);
+                    if (value === true) {
+                        $timeout(function() {
+                            element[0].focus();
+                        });
+                    }
+                });
+
+                console.log('focus on ' + attrs.focusMe);
+
+                //                    if (attrs.focusMe) {
+                //                        $timeout(functino() {
+                //                            element[0].focus();
+                //                        });
+                //                    }
             }
-        ])
-        .controller('LoginModalController', function($scope, $modal, $log) {
+        };
+    }
 
-            $scope.isOpen = false;
+    SigninModalController.$inject = ['$scope', '$modal', '$log'];
+    ModalFocusDirective.$inject = ['$timeout', '$parse'];
 
-            $scope.showLogin = function() {
-                var modalInstance = $modal.open({
-                    templateUrl: 'loginModal.html',
-                    controller: 'AuthenticationController'
-                });
-
-                modalInstance.result.then(function(result) {
-                    $log.info('Modal result %o', result);
-                    $scope.isOpen = false;
-                }, function(result) {
-                    $log.info('Modal dismissed at: ' + new Date());
-                    $scope.isOpen = false;
-                });
-
-                modalInstance.opened.then(function(args) {
-                    $scope.isOpen = true;
-                });
-
-            };
-
-
-        })
-        .directive('modalFocus', function($timeout, $parse) {
-            return {
-                restrict: 'A',
-                link: function(scope, element, attrs) {
-                    var model = $parse(attrs.modalFocus);
-
-                    scope.$watch(model, function(value) {
-                        console.log('value=', value);
-                        if (value === true) {
-                            $timeout(function() {
-                                element[0].focus();
-                            });
-                        }
-                    });
-
-                    console.log('focus on ' + attrs.focusMe);
-
-                    //                    if (attrs.focusMe) {
-                    //                        $timeout(functino() {
-                    //                            element[0].focus();
-                    //                        });
-                    //                    }
-                }
-            };
-        });
+    angular.module('users')
+        .directive('loginModal', SigninModalDirective)
+        .controller('LoginModalController', SigninModalController)
+        .directive('modalFocus', ModalFocusDirective);
 
 })();
--- modules/users/client/directives/signup-modal.client.directive.js
+++ modules/users/client/directives/signup-modal.client.directive.js
@@ -1,46 +1,48 @@
 (function() {
     'use strict';
 
+    function SignupModalDirective() {
+        return {
+            transclude: true,
+            templateUrl: 'modules/users/views/templates/signup-modal.client.template.html',
+            restrict: 'EA',
+            scope: {
+                signin: '&',
+                title: '@?'
+            },
+            controller: 'SignupModalController'
+        };
+    }
+
+    function SignupModalController($scope, $modal, $log) {
+
+        $scope.isOpen = false;
+
+        $scope.showSignup = function() {
+            var modalInstance = $modal.open({
+                templateUrl: 'signupModal.html',
+                controller: 'AuthenticationController',
+                size: 'lg'
+            });
+
+            modalInstance.result.then(function(result) {
+                $log.info('Modal result %o', result);
+                $scope.isOpen = false;
+            }, function(result) {
+                $log.info('Modal dismissed at: ' + new Date());
+                $scope.isOpen = false;
+            });
+
+            modalInstance.opened.then(function(args) {
+                $scope.isOpen = true;
+            });
+        };
+    }
+
+    SignupModalController.$inject = ['$scope', '$modal', '$log'];
+
     angular.module('users')
-        .directive('signupModal', [
-            function() {
-                return {
-                    transclude: true,
-                    templateUrl: 'modules/users/views/templates/signup-modal.client.template.html',
-                    restrict: 'EA',
-                    scope: {
-                        signin: '&',
-                        title: '@?'
-                    },
-                    controller: 'SignupModalController'
-                };
-            }
-        ])
-        .controller('SignupModalController', function($scope, $modal, $log) {
-
-            $scope.isOpen = false;
-
-            $scope.showSignup = function() {
-                var modalInstance = $modal.open({
-                    templateUrl: 'signupModal.html',
-                    controller: 'AuthenticationController',
-                    size: 'lg'
-                });
-
-                modalInstance.result.then(function(result) {
-                    $log.info('Modal result %o', result);
-                    $scope.isOpen = false;
-                }, function(result) {
-                    $log.info('Modal dismissed at: ' + new Date());
-                    $scope.isOpen = false;
-                });
-
-                modalInstance.opened.then(function(args) {
-                    $scope.isOpen = true;
-                });
-            };
-
-
-        });
+        .directive('signupModal', SignupModalDirective)
+        .controller('SignupModalController', SignupModalController);
 
 })();
