b56f346421e676e64c16444814c93217abcd7b50
Patrick Fowler <pat@outset.com>
2014-12-19 15:29:37 -0800

    Made phone number matching more flexible

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -100,7 +100,7 @@ var UserSchema = new Schema({
         type: String,
         trim: true,
         default: '',
-        match: [/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/]
+        match: [/(\+?\d{1,2}\s?-?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/]
 		// TODO: Look at https://github.com/albeebe/phoneformat.js or https://github.com/Bluefieldscom/intl-tel-input for phone # formatting
     },
 
