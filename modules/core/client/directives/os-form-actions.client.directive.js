668673dfd88a55d21b8df0c6be5b84cca972198f
Patrick Fowler <pat@outset.com>
2014-12-19 23:10:49 -0800

    New standardized "form actions" controller to allow for consistency "cancel | alt | update" operations across pages. TODO: use on all edit/create pages

--- modules/core/client/directives/os-form-actions.client.directive.js
+++ modules/core/client/directives/os-form-actions.client.directive.js
@@ -1,3 +1,58 @@
-/**
- * Created by patf on 12/19/14.
- */
+(function () {
+    'use strict';
+
+
+    function FormActionsDirective() {
+
+        var template =
+                '<div class="pull-right">' +
+                '   <input ng-if="vm.showCancel" type="button" class="btn btn-link" value="{{vm.cancelText}}" ng-click="vm.cancel()">' +
+                '   <input ng-if="vm.showAlt" type="button" class="btn btn-default" value="{{vm.altText}}" ng-click="vm.alt()">' +
+                '   <input type="submit" class="btn {{vm.submitClass}}" ng-class="{ \'disabled\': vm.disableSubmit}" value="{{vm.submitText}}">' +
+                '</div>';
+
+        var ddo = {
+            template : template,
+            scope: {
+                disableSubmit: '=?',
+                submitText: '@?',
+                submitClass: '@?',
+                cancelFn: '&?',
+                cancelText: '@?',
+                altFn: '&?',
+                altText: '@?'
+            },
+            controller: function() {
+                var vm = this;
+
+                vm.showAlt = !!vm.altFn();
+                vm.altText = vm.altText || 'Other Action';
+
+                vm.showCancel = !!vm.cancelFn();
+                vm.cancelText = vm.cancelText || 'cancel';
+
+                vm.submitClass = vm.submitClass || 'btn-success';
+                vm.submitText = vm.submitText || 'Save';
+                if(typeof vm.disableSubmit === 'undefined') {
+                    vm.disableSubmit = false;
+                }
+
+                vm.alt = function() {
+                    vm.altFn()();
+                };
+                vm.cancel = function() {
+                    vm.cancelFn()();
+                };
+            },
+            controllerAs: 'vm',
+            bindToController: true
+
+        };
+
+        return ddo;
+    }
+
+    angular.module('core')
+        .directive('osFormActions', FormActionsDirective);
+
+})();
