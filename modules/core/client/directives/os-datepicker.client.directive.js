95eabde19bf31445eb8e20662f437a49ec0fb7a0
Patrick Fowler <pat@outset.com>
2015-01-08 13:31:25 -0800

    Tweaked date picker directive to be DATE only (trying to eliminate time)

--- modules/core/client/directives/os-datepicker.client.directive.js
+++ modules/core/client/directives/os-datepicker.client.directive.js
@@ -1,52 +1,130 @@
-(function() {
+(function () {
     'use strict';
+    var y = moment().year();
 
-    angular.module('core')
-        /** @ngdoc
-         *   @description Self Contained Date Picker element for use on pages
-         *   @param {object} model       The model to render and save @required
-         *   @param {string} dp-class    CSS Class names to include @optional
-         *   @param {boolean} required   Attribute indicating that the field is required
-         *                               on the form. May be used as a standalone attibute
-         *                               or as a boolean.
-         */
-        .directive('osDatePicker', [function() {
-            return {
-                priority: 0,
-                templateUrl: '/modules/core/views/templates/os-datepicker.client.template.html',
-                replace: true,
-                restrict: 'E',
-                scope: {
-                    model: '=',
-                    dpClass: '@?',
-                    required: '=?',
-                    initialMode: '@?'
-                },
-                controller: function($scope, $element, $attrs, $transclude) {
-                    this.required = this.required === undefined && $attrs.required !== undefined || !!this.required;
+    function yearIsChill(d) {
+        return (d.year() > y - 90 && d.year() < y + 20);
+    }
 
-                    this.format = 'dd/MM/yyyy';
+    function getDate(model, format) {
+        var t;
+        if (!model || _.isEmpty(model)) {
+            return '';
+        }
+        if ((t = moment(model, format)).isValid() && yearIsChill(t)) {
+            return t;
+        }
 
-                    this.dateOptions = {
-                        formatYear: 'yyyy',
-                        startingDay: 1,
-                        showWeeks: false
-                    };
+        if (typeof model === 'string') {
+            var stripped = model.replace(/[^\d]/, '');
 
-                    this.initialMode = this.mode || 'year';
-                    console.log('Initial mode: %o', this.initialMode);
+            if ((t = moment(stripped)).isValid() && yearIsChill(t)) {
+                return t;
+            }
+            if ((t = moment(stripped, format)).isValid() && yearIsChill(t)) {
+                return t;
+            }
 
-                    this.toggle = function($event) {
-                        $event.preventDefault();
-                        $event.stopPropagation();
+            if ((t = moment(stripped, 'MMDDYYYY')).isValid() && yearIsChill(t)) {
+                return t;
+            }
 
-                        this.opened = !this.opened;
-                    };
-                },
-                controllerAs: 'ctrl',
-                bindToController: true
-            };
-        }]);
+            if ((t = moment(stripped, 'YYYYMMDD')).isValid() && yearIsChill(t)) {
+                return t;
+            }
+        }
+        if ((t = moment(model)).isValid() && yearIsChill(t)) {
+            return t;
+        }
 
+        return '';
+    }
 
-})();
+    function dpCtrl($element, $attrs, $scope, $log) {
+        var vm = this;
+
+        vm.format = vm.format || 'YYYY-MM-DD';
+        vm.display = 'L'; // Locale Specific "Date" Format
+        vm.displayFormat = moment().format('L').replace(/\d{4}/, 'YYYY').replace(/\d{2}/, 'MM').replace(/\d{2}/, 'DD');
+        vm.required = vm.required === undefined && $attrs.required !== undefined || !!vm.required;
+
+        var t = getDate(vm.model, vm.format);
+        vm.shadow = !!t ? t.format(vm.displayFormat) : t;
+
+        $scope.$watch('vm.shadow', function (newVal, oldVal) {
+            if(newVal === oldVal) { return; }
+
+            $log.info('[osDp] Change Detected, %o --> %o {%s}', oldVal, newVal, vm.format);
+
+            var m;
+
+            if (!!newVal) {
+                if(moment.isMoment(newVal)) {
+                    m = newVal;
+                } else {
+                    m = moment(newVal, 'L');
+                }
+
+                if (m.isValid()) {
+                    vm.model = m.format(vm.format);
+                }
+            }
+
+            debugger; // try to set form validity
+        });
+    }
+
+    /** @ngdoc
+     *   @description Self Contained Date Picker element for use on pages
+     *   @param {object} model       The model to render and save @required
+     *   @param {string} dp-class    CSS Class names to include @optional
+     *   @param {boolean} required   Attribute indicating that the field is required
+     *                               on the form. May be used as a standalone attibute
+     *                               or as a boolean.
+     */
+
+    function dpDirective() {
+        return {
+            priority: 0,
+            templateUrl: '/modules/core/views/templates/os-datepicker.client.template.html',
+            replace: true,
+            restrict: 'E',
+            scope: {
+                model: '=',
+                required: '=?',
+                format: '=?',
+                //id: '@?',
+                name: '@?',
+                class: '@?'
+            },
+            controller: dpCtrl,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+    }
+
+    function dpAltDirective() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+
+                if (ngModel) { // Don't do anything unless we have a model
+
+                    ngModel.$parsers.push(function (value) {
+                        return value * 100;
+                    });
+
+                    ngModel.$formatters.push(function (value) {
+                        return value / 100;
+                    });
+
+                }
+            }
+        };
+    }
+
+    angular.module('core').directive('dateInput', dpDirective);
+
+})
+();
