ced50ebbdd9fadfd3d125dd271df6297f026e58f
Patrick Fowler <pat@outset.com>
2015-01-07 21:12:41 -0800

    Added functionality to get configs from server

--- modules/core/client/services/config.client.service.js
+++ modules/core/client/services/config.client.service.js
@@ -1,3 +1,24 @@
-/**
- * Created by patf on 1/7/15.
- */
+(function() {
+    'use strict';
+
+//Drivers service used to communicate Drivers REST endpoints
+    function ConfigFactory($resource) {
+        return {
+            getStates: function() {
+                var rsrc = $resource('api/config/states');
+                return rsrc.query();
+            },
+            getBaseSchedule: function() {
+                var rsrc = $resource('api/config/baseSchedule');
+                return rsrc.query();
+            }
+        };
+    }
+
+    ConfigFactory.$inject = ['$resource'];
+
+    angular
+        .module('core')
+        .factory('AppConfig', ConfigFactory);
+
+})();
--- modules/core/server/routes/config.server.routes.js
+++ modules/core/server/routes/config.server.routes.js
@@ -2,12 +2,14 @@
 
 module.exports = function(app) {
 	// Root routing
-    var config = require('../../../../modules/core/server/controllers/config.server.controller');
+    var
+        path         = require('path'),
+        config       = require(path.resolve('./modules/core/server/controllers/config.server.controller'));
 
     app.route('/api/config')
         .get(config.getAllConfigs);
 
-    app.route('/api/config/:name')
+    app.route('/api/config/:configName')
         .get(config.read);
 
     app.param('configName', config.getConfig);
