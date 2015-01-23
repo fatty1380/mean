b5d96dad2dff81fc88fe3ee37f56d8288ad2b9ca
Patrick Fowler <pat@outset.com>
2015-01-22 18:19:48 -0800

    Minified for PRODUCTION - DOH!

--- public/lib/pdfjs-dist/build/pdf.combined.js
+++ public/lib/pdfjs-dist/build/pdf.combined.js
@@ -22,8 +22,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.1086';
-PDFJS.build = '465f52e';
+PDFJS.version = '1.0.1093';
+PDFJS.build = '801902c';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
