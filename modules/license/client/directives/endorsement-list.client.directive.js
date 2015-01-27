2c96c0cd7a1d214f5e161700cd76da5f3f6a2641
Patrick Fowler <pat@outset.com>
2015-01-26 16:05:17 -0800

    Created directive for displaying applicants

--- modules/license/client/directives/endorsement-list.client.directive.js
+++ modules/license/client/directives/endorsement-list.client.directive.js
@@ -4,7 +4,7 @@
     var template = '<span ng-repeat="(name, val) in vm.endorsements" ' +
         'ng-init="description = vm.getEndorsementName(name)" ' +
         'class="endorsement label label-success" ' +
-        'tooltip="{{description}}">{{name}}&nbsp;</span>'
+        'tooltip="{{description}}">{{name}}&nbsp;</span>';
     
     function EndorsementDisplayDirective() {
         var ddo;
@@ -13,12 +13,12 @@
             template: template,
             restrict: 'E',
             scope: {
-                endorsements: '='
+                endorsements: '=model'
             },
             controller : function() {
                 var vm = this;
                 
-                vm.endorsements = [
+                vm.endorsementBase = [
                     {key: 'HME', description: 'Hazardous Materials', value: false},
                     {key: 'P', description: 'Passenger', value: false},
                     {key: 'S', description: 'School Bus', value: false},
@@ -28,11 +28,19 @@
                 ];
 
                 vm.getEndorsementName = function(key) {
-                    var name = _.find(vm.endorsements, {key: key});
+                    var name = _.find(vm.endorsementBase, {key: key});
 
                     return name.description;
                 };
-            }
-        }
+            },
+            controllerAs: 'vm',
+            bindToController: true
+        };
+
+        return ddo;
     }
+
+    angular.module('license')
+        .directive('osetListEndorsements', EndorsementDisplayDirective);
+
 })();
