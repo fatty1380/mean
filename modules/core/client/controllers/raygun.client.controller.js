43f5bbb258ca72ef404f14d9606ff74c25402b32
Patrick Fowler <pat@outset.com>
2015-02-05 18:29:28 -0700

    RAYGUN!!!

---
+++ modules/core/client/controllers/raygun.client.controller.js
@@ -0,0 +1,28 @@
+(function() {
+    function RaygunProvider($provide) {
+        $provide.decorator("$exceptionHandler", ['$delegate', function($delegate) {
+            return function (exception, cause) {
+                Raygun.send(exception);
+                $delegate(exception, cause);
+            }
+        }])
+    }
+    RaygunProvider.$inject = ['$provide'];
+
+    angular.module('core')
+        .config(RaygunProvider);
+
+
+
+    function RaygunHandler($delegate, $log) {
+        return function (exception, cause) {
+            $log.debug('Sending to Raygun');
+            Raygun.send(exception);
+            $delegate(exception, cause);
+        }
+    };
+
+    RaygunHandler.$inject = ['$delegate', '$log'];
+
+    $provide.decorator("$exceptionHandler", RaygunHandler);
+})();
