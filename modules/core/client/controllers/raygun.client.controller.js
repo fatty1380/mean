83c2ada4b3c83a89cdea185daf4a4e6d8745d964
Patrick Fowler <pat@outset.com>
2015-02-11 10:33:43 -0800

    Refactored code to limit $location injections into '.run(*)' methods. Fixed typo

--- modules/core/client/controllers/raygun.client.controller.js
+++ modules/core/client/controllers/raygun.client.controller.js
@@ -1,26 +1,25 @@
 (function () {
-    function RaygunProvider($provide) {
-        $provide.decorator('$exceptionHandler',
-            ['$delegate', function ($delegate) {
-                return function (exception, cause) {
-                    Raygun.send(exception);
-                    $delegate(exception, cause);
-                }
-            }]);
+    'use strict';
 
-        $provide.decorator('$exceptionHandler',
-            ['$delegate', '$log',
-                function ($delegate, $log) {
-                    return function (exception, cause) {
-                        $log.debug('Sending exception to Raygun');
-                        Raygun.send(exception);
-                        $delegate(exception, cause);
-                    }
-                }]);
-
-        console.log('completed initialization of Raygun Provider(s)')
+    function RaygunDelegate($delegate, $log) {
+        return function (exception, cause) {
+            if (!/localhost/i.test(exception.sourceURL)) {
+                debugger;
+                Raygun.send(exception);
+                $log.debug('sent exception to Raygun');
+            }
+            $delegate(exception, cause);
+        };
     }
 
+    function RaygunProvider($provide) {
+        $provide.decorator('$exceptionHandler', RaygunDelegate);
+        debugger;
+
+        console.log('completed initialization of Raygun Provider(s)');
+    }
+
+    RaygunDelegate.$inject = ['$delegate', '$log'];
     RaygunProvider.$inject = ['$provide'];
 
     angular.module('core')
