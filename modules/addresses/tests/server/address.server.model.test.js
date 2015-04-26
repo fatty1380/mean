00d08c126b224feb57019e9ab7213cfc5843058d
Patrick Fowler <pat@outset.com>
2015-04-26 14:37:01 -0700

    Got Address model tests working and added virtual methods.

--- modules/addresses/tests/server/address.server.model.test.js
+++ modules/addresses/tests/server/address.server.model.test.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 var should = require('should'),
-    mongoose = require('mongoose'),
-    User = mongoose.model('User'),
-    Address = mongoose.model('Address');
+mongoose   = require('mongoose'),
+User       = mongoose.model('User'),
+Address    = mongoose.model('Address');
 
 /**
  * Globals
@@ -16,8 +16,8 @@ var address;
 /**
  * Unit tests
  */
-describe.skip('Address Model Unit Tests:', function() {
-    beforeEach(function(done) {
+describe('Address Model Unit Tests:', function () {
+    beforeEach(function (done) {
 
         address = new Address({
             type: 'main',
@@ -30,18 +30,23 @@ describe.skip('Address Model Unit Tests:', function() {
         done();
     });
 
-    describe('Method Save', function() {
-        it('should be able to save without problems', function(done) {
-            return address.save(function(err) {
-                should.not.exist(err);
-                done();
-            });
+    describe('Method Save', function () {
+        it('should be able to save without problems', function (done) {
+            return address.save()
+                .then(function (address) {
+                    console.log('saved address %j', address);
+                    done();
+                }, function (err) {
+                    console.log('got error  %j', err);
+                    should.not.exist(err);
+                    done(err);
+                });
         });
 
-        it('should be able to save multi-line addresses without problems', function(done) {
+        it('should be able to save multi-line addresses without problems', function (done) {
             address.streetAddresses.push('Unit F');
 
-            return address.save(function(err) {
+            return address.save(function (err) {
                 should.not.exist(err);
                 done();
             });
@@ -49,52 +54,99 @@ describe.skip('Address Model Unit Tests:', function() {
 
         // BEGIN Validation Checks : Error States //
 
-        it('should be able to show an error when try to save without type', function(done) {
+        it('should be able to show an error when try to save without type', function (done) {
             address.type = '';
 
-            return address.save(function(err) {
+            return address.save(function (err) {
                 should.exist(err);
                 done();
             });
         });
 
-        it('should be able to show an error when try to save without a street address', function(done) {
-            address.streetAddresses = [];
+        it('should compact empty street address lines', function (done) {
+            address.streetAddresses = ['', '123 Fake Street'];
 
-            return address.save(function(err) {
-                should.exist(err);
-                done();
-            });
+            return address.save()
+                .then(function (address) {
+                    address.should.have.property('streetAddresses').with.length(1);
+
+                    address.streetAddresses = ['', ''];
+
+                    return address.save();
+                }, done)
+                .then(function(address) {
+                    address.should.have.property('streetAddresses').with.length(0);
+                    done();
+                }, done);
         });
 
-        it('should be able to show an error when the state is not a valid state', function(done) {
+        it('should be able to show an error when the state is not a valid state', function (done) {
             address.state = 'JF';
 
-            return address.save(function(err) {
+            return address.save(function (err) {
                 should.exist(err);
                 done();
             });
         });
 
-        it('should be able to show an error when no state is defined', function(done) {
-            address.state = '';
-            return address.save(function(err) {
-                should.exist(err);
-                done();
-            });
-        });
-
-        it('should be able to show an error when a random type is defined', function(done) {
+        it('should be able to show an error when a random type is defined', function (done) {
             address.type = 'shenanigans';
 
-            return address.save(function(err) {
+            return address.save(function (err) {
                 should.exist(err);
                 done();
+            });
+        });
+
+        it('should be able to define a custom type', function (done) {
+            address.type = 'other';
+            address.typeOther = 'shenanigans';
+
+            return address.save(function (err) {
+                should.not.exist(err);
+
+                address.should.have.property('type', 'other');
+                address.should.have.property('typeOther', 'shenanigans');
+
+                done();
+            });
+        });
+
+        describe('virtual properties', function() {
+            it('should not report as empty when a normal address is saved', function(done) {
+                address = new Address();
+
+                address.save()
+                .then(function(address) {
+                        address.should.have.property('empty', true);
+                        address.should.have.property('zipOnly', false);
+                        done();
+                    }, done);
+            });
+            it('should report as empty when a blank address is saved', function(done) {
+                address.save()
+                    .then(function(address) {
+                        address.should.have.property('empty', false);
+                        address.should.have.property('zipOnly', false);
+                        done();
+                    }, done);
+
+            });
+            it('should report as zipOnly when saved with only a zip', function(done) {
+                address = new Address({zipCode: '12345'});
+
+                address.save()
+                    .then(function(address) {
+                        address.should.have.property('empty', false);
+                        address.should.have.property('zipOnly', true);
+                        done();
+                    }, done);
+
             });
         });
     });
 
-    afterEach(function(done) {
+    afterEach(function (done) {
         Address.remove().exec();
         User.remove().exec();
 
--- modules/jobs/server/models/address.server.model.js
+++ modules/jobs/server/models/address.server.model.js
@@ -4,17 +4,19 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    constants = require('../../../../modules/core/server/models/outset.constants');
+Schema       = mongoose.Schema,
+_            = require('lodash'),
+path         = require('path'),
+log          = require(path.resolve('./config/lib/logger')).child({
+    module: 'addresses',
+    file: 'Address.Model'
+}),
+constants    = require(path.resolve('./modules/core/server/models/outset.constants'));
 
-//var constants = require('core/server/models/outset.constants');
 
-/**
- * A Validation function for street addresss
- */
-var validateStreetAddressValue = function(streetAddresses) {
-    return (streetAddresses && streetAddresses.length > 0) && (streetAddresses[0].length > 3);
-};
+function validateState(val) {
+    return _.isEmpty(val) || _.contains(constants.stateAbbreviations, val);
+}
 
 /**
  * Address Schema
@@ -35,9 +37,8 @@ var AddressSchema = new Schema({
 
     streetAddresses: {
         type: [String],
-        default: [''],
+        default: [],
         trim: true
-        //validate: [validateStreetAddressValue, 'Please provide a valid street address'],
     },
 
     city: {
@@ -48,9 +49,8 @@ var AddressSchema = new Schema({
 
     state: {
         type: String,
-        default: '',
-        required: 'Please specify a State'
-        //enum: constants.stateAbbreviations(),
+        default: null,
+        validate: validateState
     },
 
     zipCode: {
@@ -59,10 +59,56 @@ var AddressSchema = new Schema({
         match: [/\d{5}(\-\d{4})?/],
         trim: true
     }
+}, {toJSON: {virtuals: true}});
+
+AddressSchema.virtual('empty')
+    .get(function () {
+        log.trace('virtual.empty', 'GET');
+        var compacted = _.compact([_.compact(this.streetAddresses).length, this.city, this.state, this.zipCode]);
+        log.trace('virtual.empty', {vals: compacted, isEmpty: _.isEmpty(compacted)});
+
+        return _.isEmpty(compacted);
+    });
+
+AddressSchema.virtual('zipOnly')
+    .get(function () {
+        log.trace('virtual.zipOnly', 'GET');
+        var others = _.compact([_.compact(this.streetAddresses).length, this.city, this.state]);
+        log.trace('virtual.zipOnly', {val: this.zipCode, zipIsEmpty: _.isEmpty(this.zipCode), others: others});
+        return !_.isEmpty(this.zipCode) && _.isEmpty(others);
+    });
+
+AddressSchema.virtual('searchString')
+    .get(function () {
+        log.trace('virtual.searchString', 'GET');
+        var street = this.streetAddresses.join(' ');
+
+        return _.compact([street, this.city, this.state, this.zipCode]).join(' ');
+    });
+
+AddressSchema.pre('init', function (next, data) {
+    console.log('init =================================================================================================');
+    log.trace('pre.init', {streetAddresses: data.streetAddresses});
+
+    this._doc.streetAddresses = _.compact(data.streetAddresses);
+
+    next();
 });
 
-AddressSchema.methods.checkConfig = function() {
-    console.log('config enums: ' + constants.usStates);
-};
+AddressSchema.pre('validate', function (next) {
+    console.log('validate =================================================================================================');
+    log.trace('pre.validate');
+
+    this.state = _.isEmpty(this.state) ? null : this.state.toUpperCase();
+    next();
+});
+
+AddressSchema.pre('save', function (next) {
+    console.log('save =================================================================================================');
+    log.trace('pre.save');
+    this.streetAddresses = _.compact(this.streetAddresses);
+    next();
+});
+
 
 mongoose.model('Address', AddressSchema);
