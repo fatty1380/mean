3f028f8df48ad2f9f22021841ee1019a584f56bc
Patrick Fowler <pat@outset.com>
2015-05-05 13:05:48 -0700

    Tweaked behavior of header to change appearance on scroll

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,37 +1,58 @@
-(function() {
+(function () {
     'use strict';
 
-    function HeaderController($scope, $state, Authentication, Menus) {
-        $scope.$state = $state;
-        $scope.authentication = Authentication;
-        $scope.navbarClass = ''; //'navbar-inverse';
+    function HeaderController($scope, $state, $document, Authentication, Menus) {
+        var vm = this;
+
+        vm.state = $state;
+        vm.auth = Authentication;
+        vm.navbarClass = ''; //'navbar-inverse';
+        vm.menu = Menus.getMenu('topbar');
+        vm.cmenu = 'topbar';
 
         // Get the topbar menu
-        if ($scope.authentication.user && $scope.authentication.isAdmin()) {
-            $scope.menu = Menus.getMenu('adminbar');
-            //$scope.navbarClass = 'navbar-inverse';
-        } else {
-            $scope.menu = Menus.getMenu('topbar');
-            //$scope.navbarClass = 'navbar-default';
-        }
-        $scope.stateLink = '';
+        var loadMenus = function() {
+            if (Authentication.isAdmin() && vm.cmenu !== 'adminbar') {
+                vm.cmenu = 'adminbar';
+                vm.menu.length = 0;
+                _.extend(vm.menu, Menus.getMenu('adminbar'));
+                //vm.navbarClass = 'navbar-inverse';
+            } else if(vm.cmenu !== 'topbar') {
+                vm.cmenu = 'topbar';
+                vm.menu.length = 0;
+
+                _.extend(vm.menu,Menus.getMenu('topbar'));
+                //vm.navbarClass = 'navbar-default';
+            }
+        };
+
+        vm.stateLink = '';
 
         // Toggle the menu items
-        $scope.isCollapsed = false;
-        $scope.toggleCollapsibleMenu = function() {
-            $scope.isCollapsed = !$scope.isCollapsed;
+        vm.isCollapsed = false;
+        vm.toggleCollapsibleMenu = function () {
+            vm.isCollapsed = !vm.isCollapsed;
         };
 
         // Collapsing the menu after navigation
-        $scope.$on('$stateChangeSuccess', function() {
-            $scope.isCollapsed = false;
-            $scope.hideHeader = !!($state.$current.data && $state.$current.data.hideHeader);
+        $scope.$on('$stateChangeSuccess', function () {
+            loadMenus();
+            vm.isCollapsed = false;
+            vm.hideHeader = !!($state.$current.data && $state.$current.data.hideHeader);
         });
 
+        var handler = function() {
+            //console.log('SCROLLING: %d', $document.scrollTop());
+            vm.scroll = $document.scrollTop();
+        };
+        $document.on('scroll', $scope.$apply.bind($scope, handler));
+        handler();
+
+        loadMenus();
     }
 
 
-    HeaderController.$inject = ['$scope', '$state', 'Authentication', 'Menus'];
+    HeaderController.$inject = ['$scope', '$state', '$document', 'Authentication', 'Menus'];
 
     angular
         .module('core')
