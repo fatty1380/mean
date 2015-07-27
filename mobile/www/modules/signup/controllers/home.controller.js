903a0f92660c021b73729f8b3d7f0bf6584ac16f
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-07-27 10:00:25 +0300

    add contacts picker to home for test

--- mobile/www/modules/signup/controllers/home.controller.js
+++ mobile/www/modules/signup/controllers/home.controller.js
@@ -1,19 +1,31 @@
 (function () {
     'use strict';
 
-    var homeCtrl = function ($scope, $state) {
-        console.log("!!!!!!");
+    var homeCtrl = function ($scope, $state, contactsService) {
 
         var vm = this;
         vm.activeSlide = 1;
 
-      /*  $scope.model =  {
-            activeSlide: 1
+        vm.data = {
+            selectedContacts : []
         };
-*/
+
+        vm.pickContact = function() {
+            console.log("pickContact!!!  ");
+            contactsService.pickContact().then(
+                function(contact) {
+                    vm.data.selectedContacts.push(contact);
+                    console.log("Selected contacts=");
+                    console.log(vm.data.selectedContacts);
+                },
+                function(failure) {
+                    console.log("Bummer.  Failed to pick a contact");
+                }
+            );
+        }
     };
 
-    homeCtrl.$inject = ['$scope', '$state'];
+    homeCtrl.$inject = ['$scope', '$state', 'contactsService'];
 
     angular
         .module('signup')
