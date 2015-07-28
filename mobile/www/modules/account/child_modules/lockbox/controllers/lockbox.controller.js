5db1d01f876fb6fe91ae5548ae8a01b4dd38107f
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-07-28 14:16:17 +0300

    add actionsheet for lockbox

--- mobile/www/modules/account/child_modules/lockbox/controllers/lockbox.controller.js
+++ mobile/www/modules/account/child_modules/lockbox/controllers/lockbox.controller.js
@@ -1,17 +1,67 @@
 (function() {
     'use strict';
 
-    function lockboxCtrl() {
+    function lockboxCtrl ( $ionicPopup, $ionicActionSheet ) {
         var vm = this;
 
         vm.docs = [
-            { name: 'Document', icon:"ico ico-documentimageev",  date:'23/11/2008' },
-            { name: 'my doc', icon:"ico ico-documentimagemvr", date:'12/10/2010' },
-            { name: 'some doc', icon:"ico ico-documentimagebg", date:'22/05/2014' }
-        ]
+            {
+                id: '1234abcd5678efab90123',
+                sku: 'mvr',
+                name: 'Motor Vehicle Report',
+                created: '2015-07-11 10:33:05',
+                url: 'http://someurl/somedocument.pdf',
+                expires: null,
+                bucket: 'outset-dev',
+                key: 'kajifpaiueh13232'
+            },
+            {
+                id: '1234abcd5678efab90112',
+                sku: 'mvr',
+                name: 'Some name',
+                created: '2015-07-11 10:33:05',
+                url: 'http://someurl/somedocument.pdf',
+                expires: null,
+                bucket: 'outset-dev',
+                key: 'kajifpaiueh13232'
+            }
+        ];
+
+        vm.addDocsPopup = function() {
+            $ionicActionSheet.show({
+                buttons: [
+                    { text: 'Take a Picture' },
+                    { text: 'Order Reports' }
+                ],
+                titleText: '<span class="title">Add documents</span>',
+                cancelText: 'Cancel',
+                cssClass: 'social-actionsheet',
+                cancel: function() {
+                    console.log("Cancel");
+                },
+                buttonClicked: function(index) {
+                    switch(index){
+                        case 0:
+                            takePicture();
+                            break;
+                        case 1:
+                            orderReports();
+                            break;
+                    }
+                    return true;
+                }
+            });
+        }
+
+        function takePicture(){
+            console.log('takePicture');
+        }
+        function orderReports(){
+            console.log('orderReports');
+        }
     }
 
-    lockboxCtrl.$inject = [];
+    lockboxCtrl.$inject = ['$ionicPopup','$ionicActionSheet'];
 
     angular
         .module('lockbox')
