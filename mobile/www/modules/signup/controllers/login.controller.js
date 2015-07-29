a4341b1b389db557d6aad0dbc48a171874bd4af0
Patrick Fowler <pat@outset.com>
2015-07-29 12:03:32 -0600

    Reformatting code ...

--- mobile/www/modules/signup/controllers/login.controller.js
+++ mobile/www/modules/signup/controllers/login.controller.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
     'use strict';
 
-    function loginCtrl ($scope, $state, $location, registerService, $ionicPopup, $ionicLoading, tokenService) {
+    function loginCtrl($scope, $state, $location, registerService, $ionicPopup, $ionicLoading, tokenService) {
         var vm = this;
 
         vm.error = "";
@@ -11,7 +11,7 @@
             password: 'sergey83mark'
         };
 
-        vm.initForm = function(scope){
+        vm.initForm = function (scope) {
             vm.form = scope;
         }
 
@@ -19,7 +19,7 @@
          * @description
          * Sign In
          */
-        vm.signIn = function(){
+        vm.signIn = function () {
 
             console.log("signIn()");
 
@@ -33,38 +33,38 @@
                 .then(function (response) {
                     $ionicLoading.hide();
 
-                    if(response.success) {
+                    if (response.success) {
                         tokenService.set('access_token', response.message.data.access_token);
                         tokenService.set('refresh_token', response.message.data.refresh_token);
                         tokenService.set('token_type', response.message.data.token_type);
                         //$location.path("account/profile");
                         $state.go('account.profile')
                         vm.error = "";
-                    }else{
+                    } else {
                         vm.error = response.message.data.error_description || "error";
                     }
                 });
         }
 
-        $scope.$on( '$ionicView.afterEnter', function () {
+        $scope.$on('$ionicView.afterEnter', function () {
             // Handle iOS-specific issue with jumpy viewport when interacting with input fields.
-            if ( window.cordova && window.cordova.plugins.Keyboard ) {
-                window.cordova.plugins.Keyboard.disableScroll( true );
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                window.cordova.plugins.Keyboard.disableScroll(true);
             }
         });
-        $scope.$on( '$ionicView.beforeLeave', function () {
-            if ( window.cordova && window.cordova.plugins.Keyboard ) {
+        $scope.$on('$ionicView.beforeLeave', function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
                 // return to keyboard default scroll state
-                window.cordova.plugins.Keyboard.disableScroll( false );
+                window.cordova.plugins.Keyboard.disableScroll(false);
             }
         });
     }
 
-    loginCtrl.$inject = ['$scope', '$state', '$location', 'registerService', '$ionicPopup', '$ionicLoading' , 'tokenService'];
+    loginCtrl.$inject = ['$scope', '$state', '$location', 'registerService', '$ionicPopup', '$ionicLoading', 'tokenService'];
 
     angular
         .module('signup')
-        .controller('loginCtrl', loginCtrl );
+        .controller('loginCtrl', loginCtrl);
 })();
 
 
