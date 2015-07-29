6be0658a9c7029df8c8f58e74fb1b5ddae3ca0a2
rykov.serge <rykov.serge@gmail.com>
2015-07-29 23:35:21 +0300

    fixed bug with creating multiple instances of the same modal

--- mobile/www/modules/app/services/app.modal.service.js
+++ mobile/www/modules/app/services/app.modal.service.js
@@ -3,7 +3,7 @@
 
     var modalService = function ($ionicModal, modalTemplates, $rootScope, lockboxShareService) {
 
-        this.show = function (modalName) {
+        this.create = function (modalName) {
             var service = this,
                 scope = $rootScope.$new(),
                 modalObject = modalTemplates[modalName];
@@ -11,19 +11,30 @@
             //todo: use injected service from modalObject
             scope.vm = lockboxShareService;
 
+            // saving this service in order to be able to call common methods
+            scope.vm.modal = this;
+
             $ionicModal
                 .fromTemplateUrl(modalObject.template, {
                     scope: scope
                 })
                 .then(function (modal) {
-                    service.modal = modal;
-                    service.modal.show();
+                    service[modalName] = modal;
+                    service[modalName].show();
                 });
-
         };
 
-        this.close = function () {
-            this.modal.hide();
+        this.show = function (modalName) {
+            var modal = this[modalName];
+
+            if(modal) modal.show();
+            else this.create(modalName);
+        };
+
+        this.close = function (modalName) {
+            var modal = this[modalName];
+
+            if(modal) modal.hide();
         };
 
     };
