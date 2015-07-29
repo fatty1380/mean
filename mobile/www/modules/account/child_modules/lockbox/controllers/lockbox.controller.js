d342ea737f5dcb04ae748b43782cb82c2cbbb789
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-07-29 17:24:58 +0300

    add lockbox document viewer

--- mobile/www/modules/account/child_modules/lockbox/controllers/lockbox.controller.js
+++ mobile/www/modules/account/child_modules/lockbox/controllers/lockbox.controller.js
@@ -1,8 +1,10 @@
 (function() {
     'use strict';
 
-    function lockboxCtrl ( $ionicPopup, $ionicActionSheet ) {
+    function lockboxCtrl ( $ionicActionSheet, $ionicModal, $scope ) {
         var vm = this;
+
+
 
         vm.docs = [
             {
@@ -20,7 +22,7 @@
                 sku: 'bg',
                 name: 'Some name',
                 created: '2015-07-11 10:33:05',
-                url: 'http://someurl/somedocument.pdf',
+                url: 'http://www.arb.ca.gov/msprog/onrdiesel/documents/multirule.pdf',
                 expires: null,
                 bucket: 'outset-dev',
                 key: 'kajifpaiueh13232'
@@ -30,12 +32,30 @@
                 sku: 'ev',
                 name: 'Some name111',
                 created: '2015-08-11 10:23:05',
-                url: 'http://someurl/somedocument.pdf',
+                url: 'http://lorempixel.com/820/1720/transport/6/sometext/',
                 expires: null,
                 bucket: 'outset-dev',
                 key: 'kajifpaiueh13232'
             }
         ];
+       // vm.modal = $ionicModal.fromTemplate('<div class="modal"><header class="bar bar-header bar-positive"> <h1 class="title">I\'m A Modal</h1><div class="button button-clear" ng-click="vm.modal.hide()"><span class="icon ion-close"></span></div></header><content has-header="true" padding="true"><p>This is a modal</p></content><footer class="bar bar-footer bar-positive"><h1 class="title">Footer</h1></footer></div>', {
+        //$scope.modal = $ionicModal.fromTemplateUrl('modules/account/child_modules/lockbox/controllers/modal.html', {
+
+        $ionicModal.fromTemplateUrl('modules/account/child_modules/lockbox/templates/modal-document.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+
+        vm.viewDoc = function(doc) {
+            console.log(" ");
+            console.log("viewDoc()");
+            console.log(vm);
+
+            vm.currentDoc = doc;
+            $scope.modal.show();
+        }
 
         vm.addDocsPopup = function() {
             $ionicActionSheet.show({
@@ -71,7 +91,7 @@
         }
     }
 
-    lockboxCtrl.$inject = ['$ionicPopup','$ionicActionSheet'];
+    lockboxCtrl.$inject = [ '$ionicActionSheet','$ionicModal',  '$scope' ];
 
     angular
         .module('lockbox')
