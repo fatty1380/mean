9b2a709dfa8486670ce970dfe09bb445e2d34516
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-08-03 11:03:10 +0300

    viwer fixes

--- mobile/www/modules/account/child_modules/lockbox/controllers/lockbox.controller.js
+++ mobile/www/modules/account/child_modules/lockbox/controllers/lockbox.controller.js
@@ -7,30 +7,11 @@
         vm.lockboxDocuments = lockboxDocuments;
         vm.addDocsPopup = lockboxDocuments.addDocsPopup;
 
-        $scope.trustSrc = function(src) {
+        vm.trustSrc = function(src) {
             return $sce.trustAsResourceUrl(src);
         };
 
-        $scope.scale = 1;
-        $scope.pages = 1;
-        $scope.instance = PDFViewerService.Instance("viewer");
-        $scope.nextPage = function() {
-            $scope.instance.nextPage();
-        };
-        $scope.prevPage = function() {
-            $scope.instance.prevPage();
-        };
-        $scope.gotoPage = function(page) {
-            $scope.instance.gotoPage(page);
-        };
-        $scope.setScale = function(v) {
-            $scope.instance.setScale(v);
-        };
-        $scope.reRender = function() {
-            $scope.instance.reRender();
-        };
-
-        $scope.onPdfEvent = function(type){
+        vm.onPdfEvent = function(type){
             console.log("   **** "+type+"  ****");
             switch(type){
                 case "loadStart":
@@ -50,12 +31,7 @@
             }
         };
 
-        $scope.pageLoaded = function(curPage, totalPages) {
-            console.log('pageLoaded ',curPage, totalPages);
-            $scope.currentPage = curPage;
-            $scope.totalPages = totalPages;
-        };
-        $scope.loadProgress = function(loaded, total, state) {
+        vm.loadProgress = function(loaded, total, state) {
             //console.log('loadProgress =', loaded, 'total =', total, 'state =', state);
             //console.log('loadProgress = ',Math.ceil(loaded/total* 100) +" %");
             var progress = Math.ceil(loaded/total* 100);
@@ -82,51 +58,10 @@
         });
 
         vm.viewDoc = function(doc) {
-            console.log(" ");
-            console.log("viewDoc()");
-            console.log(vm.lockboxDocuments);
             console.log(doc);
             vm.currentDoc = doc;
             $scope.modal.show();
-
-           /* $ionicLoading.show({
-                template: 'Loading...'
-            })*/
         }
-
-        vm.addDocsPopup = function() {
-            $ionicActionSheet.show({
-                buttons: [
-                    { text: 'Take a Picture' },
-                    { text: 'Order Reports' }
-                ],
-                titleText: '<span class="title">Add documents</span>',
-                cancelText: 'Cancel',
-                cssClass: 'social-actionsheet',
-                cancel: function() {
-                    console.log("Cancel");
-                },
-                buttonClicked: function(index) {
-                    switch(index){
-                        case 0:
-                            takePicture();
-                            break;
-                        case 1:
-                            orderReports();
-                            break;
-                    }
-                    return true;
-                }
-            });
-        }
-
-        function takePicture(){
-            console.log('takePicture');
-        }
-        function orderReports(){
-            console.log('orderReports');
-        }
-
     }
 
     lockboxCtrl.$inject = [ '$ionicActionSheet', '$ionicModal', '$scope' ,'PDFViewerService', '$sce', '$ionicLoading', 'lockboxDocuments'];
