a5714453f50cb710c90d1572b9c5b0929628debc
Rykov Serge <s.rykov@mobidev.biz>
2015-08-05 14:53:18 +0300

    endorsements ico fix

--- mobile/www/modules/account/child_modules/profile/controllers/profile.share.controller.js
+++ mobile/www/modules/account/child_modules/profile/controllers/profile.share.controller.js
@@ -4,30 +4,6 @@
     function ProfileShareCtrl(profileData) {
         var vm = this;
         vm.profileData = profileData;
-
-        vm.documents = [
-            {
-                name: 'Document',
-                date: '3/17/2015',
-                checked: false
-            },
-            {
-                name: 'Document',
-                date: '3/13/2015',
-                checked: false
-            },
-            {
-                name: 'Document',
-                date: '3/12/2015',
-                checked: false
-            },
-            {
-                name: 'Document',
-                date: '3/11/2015',
-                checked: false
-            }
-        ]
-
     }
 
     ProfileShareCtrl.$inject = ['profileData'];
