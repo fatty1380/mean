5068640d04392bf52ec3f5caab50831abc869d6a
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-08-17 11:58:43 +0300

    add geocoder

--- mobile/www/modules/account/child_modules/activity/controllers/activity.controller.js
+++ mobile/www/modules/account/child_modules/activity/controllers/activity.controller.js
@@ -5,14 +5,15 @@
         .module('activity')
         .controller('ActivityCtrl', ActivityCtrl);
 
-    ActivityCtrl.$inject = ['modalService','activityService', 'activityDetailsService', '$ionicLoading', '$scope'];
+    ActivityCtrl.$inject = ['modalService','activityService', 'activityDetailsService', '$ionicLoading', '$scope', '$compile'];
 
-    function ActivityCtrl(modalService, activityService, activityDetailsService, $ionicLoading, $scope) {
+    function ActivityCtrl(modalService, activityService, activityDetailsService, $ionicLoading, $scope, $compile) {
         var vm = this;
 
-        vm.showModal  = showModal;
-        vm.closeModal  = closeModal;
-
+        vm.showModal = showModal;
+        vm.closeModal = closeModal;
+        vm.where = '';
+        vm.num = 1;
         //vm.map = { center: { latitude: 39.904903, longitude: -75.230039 }, zoom: 2 };
 
         $ionicLoading.show({
@@ -24,59 +25,71 @@
             vm.feed = result;
         });
 
+        var latLng = new google.maps.LatLng(39.904903, -75.230039);
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 3,
+            center: latLng,
+            draggable:true,
+            sensor: true,
+            zoomControl:true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: 'Point A',
+            map: map,
+            draggable: true
+        });
 
+        function initialize() {
+            getPlaceName(latLng);
 
-/*
-        vm.map = {
-            zoom:4,
-            id: "eee",
-            events: {
-                click: function (map, eventName, originalEventArgs) {
-                    console.log(originalEventArgs);
-                    /!*  var e = originalEventArgs[0];
-                     var lat = e.latLng.lat(), lon = e.latLng.lng();
-                     var marker = {
-                     id: Date.now(),
-                     coords: {
-                     latitude: lat,
-                     longitude: lon
-                     }
-                     };
-                     $scope.map.markers.push(marker);
-                     console.log($scope.map.markers);
-                     $scope.$apply();*!/
-                }
-            },
-            center : { latitude: 39.904903, longitude: -75.230039 },
-            markerCoords : { latitude: 39.904903, longitude: -75.230039}
+            // Add dragging event listeners.
+            google.maps.event.addListener(marker, 'touchstart', function() {
+                console.log('dragstart');
+            });
+
+            google.maps.event.addListener(marker, 'drag', function() {
+            });
+
+            google.maps.event.addDomListener(map, 'click', function(e) {
+                var latlng = { lat: e.latLng.G, lng: e.latLng.K };
+                getPlaceName(latlng);
+                vm.loc = latlng;
+                console.log(vm.loc);
+            });
+            google.maps.event.addDomListener(map, 'touchstart', function() {
+                window.alert('Map was touchstart!');
+            });
+
+            google.maps.event.addListener(marker, 'touchend', function() {
+                console.log('dragend');
+            });
+
         }
-*/
 
+        function getPlaceName(latlng) {
 
-        $scope.map = {
-            zoom:4,
-            id: "eee",
-            events: {
-                click: function (map, eventName, originalEventArgs) {
-                    console.log(originalEventArgs);
-                    /*  var e = originalEventArgs[0];
-                     var lat = e.latLng.lat(), lon = e.latLng.lng();
-                     var marker = {
-                     id: Date.now(),
-                     coords: {
-                     latitude: lat,
-                     longitude: lon
-                     }
-                     };
-                     $scope.map.markers.push(marker);
-                     console.log($scope.map.markers);
-                     $scope.$apply();*/
+            if(!geocoder){
+                var geocoder = new google.maps.Geocoder;
+            }
+            geocoder.geocode({'location': latlng}, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    if (results[1]) {
+                        marker.setPosition(latlng);
+                        vm.loc = latlng;
+                        vm.where = results[1].formatted_address;
+
+                    } else {
+                        window.alert('No results found');
+                    }
+                } else {
+                    window.alert('Geocoder failed due to: ' + status);
                 }
-            },
-            center : { latitude: 39.904903, longitude: -75.230039 },
-            markerCoords : { latitude: 39.904903, longitude: -75.230039},
-            bounds: {}
-        };
+            });
+        }
+
+        initialize();
 
         function showModal(modalName, entry) {
             if(entry){
--- mobile/www/modules/account/child_modules/activity/services/activity.details.service.js
+++ mobile/www/modules/account/child_modules/activity/services/activity.details.service.js
@@ -18,13 +18,51 @@
         vm.setEntry = setEntry;
 
 
+        /*function initialize() {
+
+            var latLng = new google.maps.LatLng(-34.397, 150.644);
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 8,
+                center: latLng,
+                draggable:true,
+                sensor: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            var marker = new google.maps.Marker({
+                position: latLng,
+                title: 'Point A',
+                map: map,
+                draggable: true
+            });
+
+            // Update current position info.
+
+            // Add dragging event listeners.
+            google.maps.event.addListener(marker, 'touchstart', function() {
+                console.log('dragstart');
+            });
+
+            google.maps.event.addListener(marker, 'drag', function() {
+            });
+
+            google.maps.event.addDomListener(map, 'click', function(e) {
+                console.log(e);
+                marker.setPosition(e.latLng);
+            });
+
+            google.maps.event.addDomListener(map, 'touchstart', function() {
+                window.alert('Map was touchstart!');
+            });
+
+            google.maps.event.addListener(marker, 'touchend', function() {
+                console.log('dragend');
+                //  map.setOptions({ draggable: true })
+            });
+        }*/
+
         function setEntry(entry) {
             console.log(entry);
-            entry.id = 'new';
-            entry.zoom = 12;
-            vm.entry = entry;
-            vm.center = { latitude: vm.entry.location.coordinates[0], longitude: vm.entry.location.coordinates[1] };
-            vm.markerCoords = { latitude: vm.entry.location.coordinates[0], longitude: vm.entry.location.coordinates[1] };
+            //initialize();
         }
 
         function close(name) {
