a1002b1178fb74b9a62dafe1254f187703c2b593
Patrick Fowler <pat@outset.com>
2015-09-04 23:50:11 -0700

    Adding schema updates for "share request" and "review request" in the scope of Request Messages

--- modules/messages/server/models/message.server.model.js
+++ modules/messages/server/models/message.server.model.js
@@ -126,6 +126,31 @@ ChatSchema.pre('save', function () {
 
 mongoose.model('Chat', ChatSchema);
 
+
+/**
+ * Workflow for sending an invite request to a non-outset user:
+ * 
+ * 1. User fills out at least a valid phone or email address
+ * 2. Optionally adds first/last, display names, other phone, email addresses
+ * 3. Data is posted to server, request is saved with the following:
+ *     - Sender's ID (from)
+ *     - Contact info from 1 and 2 (contactInfo)
+ *     - Current status (new) ... [Do we need a 'sent' status]
+ *     - Whether this is a friend request or a share request (requestType)
+ *     - Message body text (text)
+ *     - Request Contents (contents) - In the case of shareRequests, used to indicate what documents are to be shared
+ *     - id/_id for tying things together
+ * 4. Depending on the available contact info, the server will send an email or SMS message
+ * 5. The message will contain a link, including the requestMessage's id
+ * 6. The recipient clicks the link, and is brought into the app.
+ * 7. The recipient must signup and add a password to activate the account.
+ * 8. The RequestMessage is updated with a valid UserID in the 'to' field.
+ * 
+ * Other Points:
+ * Once signed up, the server should search for existing Requests and rectify them per step #8
+ * The same will be true if the user signs up for Outset without a link.
+ */
+
 // TODO: Combine with "Message" schema
 // var sampleContactInfo =
 // {
@@ -148,7 +173,7 @@ var RequestMessageSchema = new Schema({
     contactInfo: {
         type: Schema.Types.Mixed
     },
-    inviteContactInfo: {
+    contents: {
         type: Schema.Types.Mixed
     },
     status: {
@@ -158,7 +183,7 @@ var RequestMessageSchema = new Schema({
     },
     requestType: {
         type: String,
-        enum: ['friendRequest'],
+        enum: ['friendRequest', 'shareRequest', 'reviewRequest'],
         default: 'friendRequest'
     },
     text: {
