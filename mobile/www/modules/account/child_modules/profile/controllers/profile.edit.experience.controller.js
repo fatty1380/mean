7dc7ef5a6cc9684e4ff13af87b5352cc1da7542a
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-09-07 16:27:32 +0300

    updated files for profile  experience

---
+++ mobile/www/modules/account/child_modules/profile/controllers/profile.edit.experience.controller.js
@@ -0,0 +1,39 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('account')
+        .controller('ProfileEditExperienceCtrl', ProfileEditExperienceCtrl);
+
+    ProfileEditExperienceCtrl.$inject = ['$q', 'experienceService', 'parameters'];
+
+    function ProfileEditExperienceCtrl($q, experienceService, parameters) {
+        var vm = this;
+
+        console.log(parameters);
+
+        vm.experience = parameters;
+
+        vm.cancel = cancel;
+        vm.saveExperience = saveExperience;
+
+        function saveExperience() {
+            console.log(' ');
+            console.log('saveExperience()');
+            console.log(vm.experience);
+
+            experienceService.updateUserExperience(vm.experience._id, vm.experience)
+            .then(function (resp) {
+                vm.experience = {};
+                console.warn('success --->>>', resp);
+                cancel();
+            }, function (err) {
+                console.warn('err --->>>', err);
+            });
+        }
+
+        function cancel() {
+            vm.closeModal(null);
+        }
+    }
+})();
