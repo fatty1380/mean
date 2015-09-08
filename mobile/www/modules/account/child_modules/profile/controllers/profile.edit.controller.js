59187495fb8adcca8f099ff00c9d57aef4bf9548
Patrick Fowler <pat@outset.com>
2015-09-08 12:25:20 -0700

    Move null checking to be above the place where null exceptions are most likely

--- mobile/www/modules/account/child_modules/profile/controllers/profile.edit.controller.js
+++ mobile/www/modules/account/child_modules/profile/controllers/profile.edit.controller.js
@@ -10,6 +10,10 @@
     function ProfileEditCtrl(truckService, $state, $filter, userService, profileModalsService, trailerService, tokenService) {
         var vm = this;
 
+        if (!userService.profileData) {
+            return $state.go('home');
+        }
+        
         vm.profileData = userService.profileData;
         vm.profileData.props = vm.profileData.props || {};
         vm.started = getFormattedDate(vm.profileData.props.started);
@@ -21,10 +25,6 @@
         vm.showProfileEditTrucksModal = showProfileEditTrucksModal;
         vm.updateTrailers = updateTrailers;
         vm.updateTrucks = updateTrucks;
-
-        if (!vm.profileData) {
-            return $state.go('home');
-        }
 
         function updateTrailers (trailers) {
             if(!trailers) return;
