e51c86b352acc0e267e2004da102b980f620c65c
Sergey Markov (Kharkiv) <s.markov@mobidev.biz>
2015-09-08 15:57:05 +0300

    minor fix

--- mobile/www/modules/account/child_modules/profile/controllers/profile.edit.controller.js
+++ mobile/www/modules/account/child_modules/profile/controllers/profile.edit.controller.js
@@ -10,10 +10,6 @@
     function ProfileEditCtrl(truckService, $state, $filter, userService, profileModalsService, trailerService, tokenService) {
         var vm = this;
         
-        if (!vm.profileData) {
-            return $state.go('home');
-        }
-        
         vm.profileData = userService.profileData;
         vm.profileData.props = vm.profileData.props || {};
         vm.started = getFormattedDate(vm.profileData.props.started);
@@ -25,6 +21,10 @@
         vm.showProfileEditTrucksModal = showProfileEditTrucksModal;
         vm.updateTrailers = updateTrailers;
         vm.updateTrucks = updateTrucks;
+
+        if (!vm.profileData) {
+            return $state.go('home');
+        }
 
         function updateTrailers (trailers) {
             if(!trailers) return;
