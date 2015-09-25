b3e2ad4c856bea6fb237978f37de1e716531781f
Rykov Serge <s.rykov@mobidev.biz>
2015-09-25 20:29:01 +0300

    added webView scroll when keyboard is open, stoping app updates service when user logged out

--- mobile/www/modules/app/app.config.js
+++ mobile/www/modules/app/app.config.js
@@ -2,7 +2,7 @@
 
 var AppConfig = (function () {
     var appModuleName = 'outset',
-        appModuleDependencies = ['ionic', 'ui.router'],
+        appModuleDependencies = ['ionic', 'ui.router', 'ngIOS9UIWebViewPatch'],
         registerModule = function (moduleName, dependencies) {
             // create angular module
             angular.module(moduleName, dependencies || []);
