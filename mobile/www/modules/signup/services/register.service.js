77a97fe1c080669c2b58606bef0516efc69f554a
Patrick Fowler <pat@outset.com>
2015-10-01 00:17:05 -0700

    Split out "API" service into its own file, out of the 'register service' file

--- mobile/www/modules/signup/services/register.service.js
+++ mobile/www/modules/signup/services/register.service.js
@@ -5,53 +5,6 @@
         .module('signup')
         .factory('registerService', registerService);
     
-    angular.module(AppConfig.appModuleName)
-        .factory('API', serverConnectionService);
-        
-    function serverConnectionService($http) {
-        var service = {
-            serialize: serializeData,
-            doRequest: doApiRequest
-        };
-        
-        return service;
-        
-        //////////////////////////////////////////////////////
-        
-        function doApiRequest(apiUrl, method, data, needSerialize) {
-            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-            return $http ({
-                url: apiUrl,
-                method: method,
-                data: !needSerialize ? serializeData(data) : data
-            })
-        }
-
-        function serializeData( data ) {
-            if ( ! angular.isObject( data ) ) {
-                return( ( data == null ) ? '' : data.toString() );
-            }
-            var buffer = [];
-            for ( var name in data ) {
-                if ( ! data.hasOwnProperty( name ) ) {
-                    continue;
-                }
-                var value = data[ name ];
-                buffer.push(
-                    encodeURIComponent( name ) +
-                    '=' +
-                    encodeURIComponent( ( value == null ) ? '' : value )
-                );
-            }
-            var source = buffer
-                    .join( '&' )
-                    .replace( /%20/g, '+' )
-                ;
-            return( source );
-        }
-    }
-
-
     registerService.$inject = ['$http', 'settings', 'API'];
     function registerService ($http, settings, API) {
 
