81276be9a1c1f8364603b668ac6cc8c6de714ef0
Patrick Fowler <pat@outset.com>
2015-10-22 21:37:27 -0700

    Here's the big one - change to the "PRODUCTION" URL!!!

--- mobile/www/modules/app/constants/settings.js
+++ mobile/www/modules/app/constants/settings.js
@@ -3,7 +3,8 @@
 
     var settings = {};
 
-    settings.baseUrl = 'http://outset-d.elasticbeanstalk.com/';
+    // settings.baseUrl = 'http://outset-d.elasticbeanstalk.com/';
+    settings.baseUrl = 'https://app.truckerline.com/';
 
     // controllers
     settings.oauth = settings.baseUrl + 'oauth/';
