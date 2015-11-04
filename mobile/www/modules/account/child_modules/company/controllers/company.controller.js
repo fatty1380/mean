da2bfbfb134fd03cbf25270bb365973ec87d5beb
Patrick Fowler <pat@outset.com>
2015-11-03 22:58:04 -0800

    Formatting for consistent Company Display

--- mobile/www/modules/account/child_modules/company/controllers/company.controller.js
+++ mobile/www/modules/account/child_modules/company/controllers/company.controller.js
@@ -6,31 +6,38 @@
         .controller('CompanyCtrl', CompanyCtrl);
 
     CompanyCtrl.$inject = ['$ionicLoading', '$ionicPopup', '$ionicHistory', '$state',
-        'company', 'jobs', 'feed', 'CompanyService'];
-        
+        'company', 'jobs', 'feed', 'userService', 'CompanyService'];
+
     function CompanyCtrl($ionicLoading, $ionicPopup, $ionicHistory, $state,
-        company, jobs, feed, CompanyService) {
+        company, jobs, feed, userService, CompanyService) {
         var vm = this;
-        
+
         vm.follow = follow;
+        vm.unfollow = unfollow;
         vm.goBack = goBack;
-        
+
 
         initialize();
         
         /////////////////////////////////////////////////////////////////////////////////////
 
         function initialize() {
-            
+
             if (_.isEmpty(company)) {
                 $ionicLoading.show({ template: 'Not Found', duration: 2500 });
                 return goBack();
             }
-            
+
             vm.company = company;
             vm.jobs = jobs || [];
             vm.feed = feed || [];
-                
+
+            userService.getUserData().then(
+                function (user) {
+                    vm.isFollowing = _.contains(vm.company.followers || [], user.id);
+                });
+
+
             $ionicLoading.hide();
         }
 
@@ -39,15 +46,30 @@
                 debugger;
                 return $state.go('account.activity');
             }
-            
+
             return $ionicHistory.goBack();
         }
 
         function follow() {
-            $ionicLoading.show({
-                template: '<i class="icon ion-checkmark"></i><br>Following',
-                duration: 2000
-            })
+            CompanyService.follow(vm.company.id)
+                .then(function success(result) {
+                    $ionicLoading.show({
+                        template: '<i class="icon ion-checkmark"></i><br>Following',
+                        duration: 1500
+                    })
+                    vm.isFollowing = true;
+                });
+        }
+
+        function unfollow() {
+            CompanyService.unfollow(vm.company.id).then(
+                function success(result) {
+                    $ionicLoading.show({
+                        template: '<i class="icon ion-close"></i>',
+                        duration: 1500
+                    })
+                    vm.isFollowing = false;
+                });
         }
     }
 
