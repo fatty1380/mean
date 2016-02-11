fba2d824406eb8f5003e3b216cfff0c15f15246e
Patrick Fowler <pat@outset.com>
2016-02-11 02:38:27 -0800

    Android Specific (mostly) build setup

--- mobile/buildAndroidAPK.sh
+++ mobile/buildAndroidAPK.sh
@@ -7,5 +7,5 @@ jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android-release
 
 mkdir -p ./platforms/android/build/archives
 
-$ANDROID_HOME/build-tools/23.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk ./platforms/android/build/archives/TruckerLine_x86_121_rc2.apk
-$ANDROID_HOME/build-tools/23.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk ./platforms/android/build/archives/TruckerLine_arm_121_rc2.apk
+$ANDROID_SDK_ROOT/build-tools/23.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk ./platforms/android/build/archives/TruckerLine_x86_121_rc2.apk
+$ANDROID_SDK_ROOT/build-tools/23.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk ./platforms/android/build/archives/TruckerLine_arm_121_rc2.apk
