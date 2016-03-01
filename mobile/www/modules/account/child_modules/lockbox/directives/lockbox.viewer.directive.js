1ee61ff01dca0ebafd887702cb0a2b342c2a1291
Patrick Fowler <pat@truckerline.com>
2016-03-01 00:47:00 -0800

    TLINE-137 #comment checking in changes to a 1.2.2.103 hotfix branch to minimize changes

--- mobile/www/modules/account/child_modules/lockbox/directives/lockbox.viewer.directive.js
+++ mobile/www/modules/account/child_modules/lockbox/directives/lockbox.viewer.directive.js
@@ -9,7 +9,7 @@
         .controller('DocumentModalCtrl', DocumentModalCtrl)
         .directive('osetDocView', ViewDocumentDirective);
 
-    function ViewDocumentDirective() {
+    function ViewDocumentDirective () {
         var directive = {
             link: link,
             template: documentTemplate,
@@ -26,7 +26,7 @@
         return directive;
 
         // Inject controller as 'vm' for consistency
-        function link(scope, el, attr, vm) {
+        function link (scope, el, attr, vm) {
             // Set defaults
             vm.scope = scope;
 
@@ -38,23 +38,23 @@
     }
 
     ViewDocDirectiveCtrl.$inject = ['modalService'];
-    function ViewDocDirectiveCtrl(DocPreview) {
+    function ViewDocDirectiveCtrl (DocPreview) {
         var vm = this;
 
         vm.openPreview = showDocument;
 
         activate();
-		
-        ///
-		
-        function activate() {
+
+        // /
+
+        function activate () {
             vm.btnText = vm.btnText || 'View';
         }
 
-        function showDocument(document) {
+        function showDocument (document) {
             return DocPreview.show('modules/account/child_modules/lockbox/templates/modal-preview.html',
                 'DocumentModalCtrl as vm',
-                document)
+                document);
         }
     }
 
@@ -62,8 +62,8 @@
 
 
     // AKA: ContactDialogCtrl
-    DocumentModalCtrl.$inject = ['parameters', '$sce', '$ionicPopup', 'LoadingService'];
-    function DocumentModalCtrl(parameters, $sce, $ionicPopup, LoadingService) {
+    DocumentModalCtrl.$inject = ['parameters', '$sce', '$timeout', '$ionicPopup', 'LoadingService', 'PDFViewerService'];
+    function DocumentModalCtrl (parameters, $sce, $timeout, $ionicPopup, LoadingService, PDFViewerService) {
         var vm = this;
 
         vm.document = parameters.document || parameters;
@@ -84,59 +84,74 @@
         vm.toggleFullScreen = toggleFullScreen;
         vm.close = close;
 
-        function close(e) {
+
+
+        function close (e) {
             e.stopPropagation();
             vm.closeModal(null);
 
             vm.fullScreenMode = false;
-            if(angular.isFunction(screen.lockOrientation)){
+            if (angular.isFunction(screen.lockOrientation)) {
                 screen.lockOrientation('portrait');
             }
         }
 
         function enlarge (e) {
             vm.fullScreenMode = !vm.fullScreenMode;
-            if(angular.isFunction(screen.lockOrientation)){
+            if (angular.isFunction(screen.lockOrientation)) {
                 screen.lockOrientation('landscape');
+            }
+
+            if (!_.isEmpty(vm.pdfInstance)) {
+                $timeout(function () {
+                    vm.pdfInstance.setScale(2);
+                }, 500);
             }
         }
 
         function minimize () {
             vm.fullScreenMode = !vm.fullScreenMode;
-            if(angular.isFunction(screen.lockOrientation)){
+            if (angular.isFunction(screen.lockOrientation)) {
                 screen.lockOrientation('portrait');
+            }
+
+            if (!_.isEmpty(vm.pdfInstance)) {
+                $timeout(function () {
+                    vm.pdfInstance.reRender();
+                }, 500);
             }
         }
 
         function toggleFullScreen () {
-            if(!vm.fullScreenMode) return;
-            vm.showControls = !vm.showControls
+            if (!vm.fullScreenMode) return;
+            vm.showControls = !vm.showControls;
         }
 
         logger.debug('Modal Visible for %s', vm.document.sku);
-        
+
         var docURL = vm.document.url;
-        
+
         if (docURL.indexOf('.pdf') > 0) {
+            vm.viewerId = vm.document.id;
             vm.pdfURL = { src: vm.document.url };
         } else {
             vm.image = vm.document.url;
         }
-                
-        //logger.debug('Modal Visible for PDF: %s IMG: %o', vm.pdfURL, vm.image);
+
+        // logger.debug('Modal Visible for PDF: %s IMG: %o', vm.pdfURL, vm.image);
 
         logger.debug('[DocumentModalCtrl] for document: %o', vm.document);
 
-        function trustSrc(src) {
-            //logger.debug('SCE Trusting resource: `%s`', src);
+        function trustSrc (src) {
+            // logger.debug('SCE Trusting resource: `%s`', src);
             return $sce.trustAsResourceUrl(src);
-        };
+        }
 
 
-        function onImageEvent(type) {
-            logger.debug("   **** onImageEvent  " + type + " ****");
+        function onImageEvent (type) {
+            logger.debug('   **** onImageEvent  ' + type + ' ****');
             switch (type) {
-                case "loadStart":
+                case 'loadStart':
                     LoadingService.showLoader('Loading Image, Please Wait.');
                     break;
                 case 'loadComplete':
@@ -150,17 +165,18 @@
                     LoadingService.hide();
                     break;
             }
-        };
+        }
 
 
-        function onPdfEvent(type) {
-            logger.debug("   **** " + type + " ****");
+        function onPdfEvent (type) {
+            logger.debug('   **** ' + type + ' ****');
             switch (type) {
-                case "loadStart":
+                case 'loadStart':
                     LoadingService.showLoader('Loading PDF, Please Wait.');
                     break;
                 case 'loadComplete':
                     LoadingService.hide();
+                    vm.pdfInstance = PDFViewerService.Instance(vm.viewerId);
                     break;
                 case 'loadError':
                     LoadingService.showAlert('Sorry, Please, try later.');
@@ -170,16 +186,16 @@
                     LoadingService.hide();
                     break;
             }
-        };
+        }
 
 
-        function loadProgress(loaded, total, state) {
-            
+        function loadProgress (loaded, total, state) {
+
             var progress = Math.ceil(loaded / total * 100);
             if (progress <= 100) {
                 vm.loadingProgress = Math.ceil(loaded / total * 100);
             }
-        };
+        }
     }
 
 
