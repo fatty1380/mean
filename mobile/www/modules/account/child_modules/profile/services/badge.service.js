40b03966089a6f3e47974d0180cc633da007c5e9
James Robinson <james@truckerline.com>
2016-03-23 11:37:38 -0700

    TLINE-278 #comment improved stub badgeing in app

--- mobile/www/modules/account/child_modules/profile/services/badge.service.js
+++ mobile/www/modules/account/child_modules/profile/services/badge.service.js
@@ -10,14 +10,15 @@
     function badgeService($http, $q) {
 
         function getBadgeLevel() {
-            return Math.floor(Math.random() * 3);
+            return Math.floor(Math.random() * 4);
         }
 
         function getIconClass() {
             var badges = {
                 0: 'bronze',
                 1: 'silver',
-                2: 'gold'
+                2: 'gold',
+                4: 'none'
             };
 
             return badges[getBadgeLevel()];
