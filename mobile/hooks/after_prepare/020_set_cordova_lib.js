ba519ed368f3a1256d70c1f37a241e56d583c2e4
Patrick Fowler <pat@truckerline.com>
2016-03-24 14:25:40 -0700

    Checking in latest Xcode configuration files

--- mobile/hooks/after_prepare/020_set_cordova_lib.js
+++ mobile/hooks/after_prepare/020_set_cordova_lib.js
@@ -14,75 +14,84 @@ var path = require('path');
 var rootdir = process.argv[2];
 
 function addPlatformBodyTag(indexPath, platform) {
-  // add the platform class to the body tag
+    process.stdout.write('Reviewing iOS Tag at Path: `' + indexPath + '`\n');
+    // add the platform class to the body tag
     try {
         var cordovaSrc = 'lib/ngCordova/dist/ng-cordova.js';
         var mocksSrc = 'lib/ngCordova/dist/ng-cordova-mocks.js';
 
-    var html = fs.readFileSync(indexPath, 'utf8');
+        var html = fs.readFileSync(indexPath, 'utf8');
 
-    var cordovaTag = findCordovaTag(html);
-    process.stdout.write('Cordova Tag: `' + cordovaTag + '`\n'); 
-    if(!cordovaTag) return; // no opening body tag, something's wrong
+        var cordovaTag = findCordovaTag(html);
+        process.stdout.write('Cordova Tag: `' + cordovaTag + '`\n');
+        if (!cordovaTag) return; // no opening body tag, something's wrong
 
-    if(cordovaTag.indexOf(cordovaSrc) > -1) return; // already added
+        if (cordovaTag.indexOf(cordovaSrc) > -1) return; // already added
 
-    process.stdout.write('Cordova Mocks Present ... Removing\n');        
-    var newCordovaTag = cordovaTag;
+        process.stdout.write('Cordova Mocks Present ... Removing\n');
+        var newCordovaTag = cordovaTag;
 
-    var srcAttr = findSrcAttr(cordovaTag);
-    if(srcAttr) {
-      newCordovaTag = cordovaTag.replace(mocksSrc, cordovaSrc);
-    } 
+        var srcAttr = findSrcAttr(cordovaTag);
+        if (srcAttr) {
+            newCordovaTag = cordovaTag.replace(mocksSrc, cordovaSrc);
+        }
 
-    html = html.replace(cordovaTag, newCordovaTag);
+        html = html.replace(cordovaTag, newCordovaTag);
 
-    fs.writeFileSync(indexPath, html, 'utf8');
+        fs.writeFileSync(indexPath, html, 'utf8');
 
-    process.stdout.write('Set Cordova Source: ' + cordovaSrc + '\n');
-  } catch(e) {
-    process.stdout.write(e);
-  }
+        process.stdout.write('Set Cordova Source: ' + cordovaSrc + '\n');
+    } catch (e) {
+        process.stdout.write(e);
+    }
 }
 
 function findCordovaTag(html) {
-  // get the body tag
-  try{
-    return html.match(/<script .*(ng-cordova.*\.js).*>/gi)[0];
-  }catch(e){}
+    // get the body tag
+    try {
+        return html.match(/<script .*(ng-cordova.*\.js).*>/gi)[0];
+    } catch (e) { }
 }
 
 function findSrcAttr(bodyTag) {
-  // get the body tag's class attribute
-  try{
-    return bodyTag.match(/ src=["|'](.*?)["|']/gi)[0];
-  }catch(e){}
+    // get the body tag's class attribute
+    try {
+        return bodyTag.match(/ src=["|'](.*?)["|']/gi)[0];
+    } catch (e) { }
 }
 
 if (rootdir) {
 
-  // go through each of the platform directories that have been prepared
-  var platforms = (process.env.CORDOVA_PLATFORMS ? process.env.CORDOVA_PLATFORMS.split(',') : []);
+    // go through each of the platform directories that have been prepared
+    var platforms = (process.env.CORDOVA_PLATFORMS ? process.env.CORDOVA_PLATFORMS.split(',') : []);
 
-  for(var x=0; x<platforms.length; x++) {
-    // open up the index.html file at the www root
-    try {
-      var platform = platforms[x].trim().toLowerCase();
-      var indexPath;
+    for (var x = 0; x < platforms.length; x++) {
+        // open up the index.html file at the www root
+        try {
+            var platform = platforms[x].trim().toLowerCase();
+            var indexPath;
 
-      if(platform == 'android') {
-        indexPath = path.join('platforms', platform, 'assets', 'www', 'index.html');
-      } else {
-        indexPath = path.join('platforms', platform, 'www', 'index.html');
-      }
+            if (platform == 'android') {
+                indexPath = path.join('platforms', platform, 'assets', 'www', 'index.html');
+            } else {
+                indexPath = path.join('platforms', platform, 'www', 'index.html');
+            }
 
-      if(fs.existsSync(indexPath)) {
-        addPlatformBodyTag(indexPath, platform);
-      }
+            if (fs.existsSync(indexPath)) {
+                addPlatformBodyTag(indexPath, platform);
+            }
 
-    } catch(e) {
-      process.stdout.write(e);
+            process.stdout.write('SET CORDOVA LIB : Round 2 --------------------------------\n');
+
+
+            if (fs.existsSync(indexPath)) {
+                addPlatformBodyTag(indexPath, platform);
+            }
+
+
+        } catch (e) {
+            process.stdout.write(e);
+        }
     }
-  }
 
 }
